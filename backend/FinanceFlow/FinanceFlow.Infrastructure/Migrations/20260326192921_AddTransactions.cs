d6a1739d47f4f02c268e1febfceb5180eea55af0
Aurel Lossou <aurelth@icloud.com>
2026-03-26 16:39:05 -0300

    feat: migration de transações adicionada com configuração Fluent API

---
+++ backend/FinanceFlow/FinanceFlow.Infrastructure/Migrations/20260326192921_AddTransactions.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace FinanceFlow.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddTransactions : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
