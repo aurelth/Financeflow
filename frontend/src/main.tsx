58b85d84514233d63eed9712811eb62f2d075e3e
Aurel Lossou <aurelth@icloud.com>
2026-03-30 11:35:38 -0300

    fix: removido ReactQueryDevtools da interface

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,7 +1,6 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
-import { ReactQueryDevtools } from '@tanstack/react-query-devtools'
 import { Toaster } from 'sonner'
 import AppRouter from './routes/AppRouter'
 import './index.css'
@@ -31,8 +30,7 @@ createRoot(document.getElementById('root')!).render(
             color: '#f1f5f9',
           },
         }}
-      />
-      <ReactQueryDevtools initialIsOpen={false} />
+      />      
     </QueryClientProvider>
   </StrictMode>
 )
