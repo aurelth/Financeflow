4b2b7de54ef709838eee6d0ee12c21a44ed81a6d
Aurel Lossou <aurelth@icloud.com>
2026-04-02 16:31:45 -0300

    fix: import não utilizado removido do Header

--- frontend/src/components/layout/Header.tsx
+++ frontend/src/components/layout/Header.tsx
@@ -9,7 +9,6 @@ import {
   DropdownMenuSeparator,
   DropdownMenuTrigger,
 } from '@/components/ui/dropdown-menu'
-import { Button } from '@/components/ui/button'
 import { useNavigate } from 'react-router-dom'
 import NotificationDropdown from '@/features/notifications/components/NotificationDropdown' // adicionado
 
